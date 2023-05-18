a56060a15d8da68d0c675771c61a52bcc2b33d2d
TrongBinhLe <ltbinhbk2010@gmail.com>
2023-05-18 14:54:42 +0700

    feature: The Budget App was created to research how to use the Core Data with swift language.
    Body: Create the project and adding the coredata model

---
+++ BudgetApp/BudgetCategoriesTableViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BudgetApp
+//
+//  Created by admin on 18/05/2023.
+//
+
+import UIKit
+
+class BudgetCategoriesTableViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemBackground
+        
+    }
+
+
+}
+
