8113afd831746026c39ed8bb9f5cee0da0d3577b
TrongBinhLe <ltbinhbk2010@gmail.com>
2023-05-18 16:15:50 +0700

    feature: Setting up core data stack

--- BudgetApp/BudgetCategoriesTableViewController.swift
+++ BudgetApp/BudgetCategoriesTableViewController.swift
@@ -6,9 +6,21 @@
 //
 
 import UIKit
+import CoreData
 
 class BudgetCategoriesTableViewController: UIViewController {
-
+    
+    private var persistentContainer: NSPersistentContainer
+    
+    init(persistentContainer: NSPersistentContainer) {
+        self.persistentContainer = persistentContainer
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .systemBackground
