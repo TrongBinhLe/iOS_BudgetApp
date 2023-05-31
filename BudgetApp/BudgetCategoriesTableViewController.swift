80589bb2e465244bd45bd00f6f845c0819f221be
TrongBinhLe <ltbinhbk2010@gmail.com>
2023-05-31 17:09:07 +0700

    Deleting Budget Category

--- BudgetApp/BudgetCategoriesTableViewController.swift
+++ BudgetApp/BudgetCategoriesTableViewController.swift
@@ -68,6 +68,19 @@ class BudgetCategoriesTableViewController: UITableViewController {
     private func layout() {
         
     }
+    
+    private func deleteBudgetCategory(_ budgetCategory: BudgetCategory) {
+        persistentContainer.viewContext.delete(budgetCategory)
+        do {
+            try persistentContainer.viewContext.save()
+        } catch {
+            // Alert error
+//            let alert = UIAlertController(title: "Error", message: "Unable to delete budget category", preferredStyle: .alert)
+//            alert.addAction(UIAlertAction(title: "OK", style: .default))
+//            present(alert, animated: true)
+            showAlert(title: "Error", message: "Unable to delete budget category")
+        }
+    }
         
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return (fetchResultsController.fetchedObjects ?? []).count
@@ -89,6 +102,14 @@ class BudgetCategoriesTableViewController: UITableViewController {
         let budgetCategory = fetchResultsController.object(at: indexPath)
         // perform navigation
         navigationController?.pushViewController(BudgetDetailsViewController(budgetCategory: budgetCategory, persistentContainer: persistentContainer), animated: true)
+    }
+    
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        
+        if editingStyle == .delete {
+            let budgetCategory = fetchResultsController.object(at: indexPath)
+            deleteBudgetCategory(budgetCategory)
+        }
     }
 }
 
---
+++ BudgetApp/Extensions/UIViewController+Extensions.swift
@@ -0,0 +1,17 @@
+//
+//  UIViewController+Extensions.swift
+//  BudgetApp
+//
+//  Created by admin on 31/05/2023.
+//
+
+import Foundation
+import UIKit
+
+extension UIViewController {
+    func showAlert(title: String, message: String) {
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "OK", style: .default))
+        present(alert, animated: true)
+    }
+}
